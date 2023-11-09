bd9d2e1ea6b15b98271a653db58d51285b771c03
Raymond Oluoch <rasholuoch@gmail.com>
2023-11-09 20:16:05 +0300

    pagination and sorting

--- src/main/java/com/iamraymond/blog/service/PostService.java
+++ src/main/java/com/iamraymond/blog/service/PostService.java
@@ -1,13 +1,14 @@
 package com.iamraymond.blog.service;
 
 import com.iamraymond.blog.payload.PostDto;
+import com.iamraymond.blog.payload.PostResponse;
 
 import java.util.List;
 
 public interface PostService {
     PostDto createPost(PostDto postDto);
 
-    List<PostDto> getAllPosts();
+    PostResponse getAllPosts(int pageNo, int pageSize, String sortBy, String sortDir);
 
     PostDto getPostById(long id);
 
